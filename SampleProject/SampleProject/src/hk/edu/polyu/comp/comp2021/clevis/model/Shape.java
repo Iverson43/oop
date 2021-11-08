2ab24838517ff5bda85d4d9d5d6bdb063a1ff06b
borisPMC <borisleung30@gmail.com>
2021-11-08 17:01:46 +0800

    Coordination

--- SampleProject/SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/model/Shape.java
+++ SampleProject/SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/model/Shape.java
@@ -1,39 +1,49 @@
 package hk.edu.polyu.comp.comp2021.clevis.model;
 
-public class Shape {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public abstract class Shape {
 
     // The name and anchor of a Shape
     private String name;
-    private double x;
-    private double y;
+    private Coordination topLeft;
 
     Shape(String n, double a, double b) {
         this.name = n;
-        this.x = a;
-        this.y = b;
+        this.topLeft = new Coordination(a,b);
     };
 
     //Returns the name and coordination of a Shape
     String getName() {return name;}
-    double getStartX() {return x;}
-    double getStartY() {return y;}
+    double getStartX() {return topLeft.getX();}
+    double getStartY() {return topLeft.getY();}
+    Coordination getTopLeft() {return topLeft;}
 
+    abstract ArrayList<Coordination> getPoints();
 }
 
 class Line extends Shape{
 
-    private double endX;
-    private double endY;
+    private Coordination bottomRight;
 
     Line(String n, double x1, double y1, double x2, double y2) {
         super(n, x1, y1);
-        this.endX = x2;
-        this.endY = y2;
+        this.bottomRight = new Coordination(x2,y2);
     }
 
     // Returns width and height of a Rectangle
-    double getEndX() {return endX;}
-    double getEndY() {return endY;}
+    double getEndX() {return bottomRight.getX();}
+    double getEndY() {return bottomRight.getY();}
+
+    ArrayList<Coordination> getPoints() {
+        ArrayList<Coordination> result = new ArrayList<Coordination>();
+        result.add(this.getTopLeft());
+        result.add(bottomRight);
+        return result;
+    }
 }
 
 class Circle extends Shape{
@@ -48,6 +58,11 @@ class Circle extends Shape{
 
     //Return the radian of a circle
     double getRadian() {return radian;}
+    ArrayList<Coordination> getPoints() {
+        ArrayList<Coordination> result = new ArrayList<>();
+        result.add(this.getTopLeft());
+        return result;
+    }
 }
 
 class Rectangle extends Shape {
@@ -65,6 +80,23 @@ class Rectangle extends Shape {
     // Returns width and height of a Rectangle
     double getWidth() {return width;}
     double getHeight() {return height;}
+
+    ArrayList<Coordination> getPoints() {
+        ArrayList<Coordination> result = new ArrayList<>();
+        result.add(this.getTopLeft());
+        result.add(new Coordination(this.getStartX()+width, this.getStartY()));
+        result.add(new Coordination(this.getStartX(), this.getStartY()+height));
+        result.add(new Coordination(this.getStartX()+width, this.getStartY()+height));
+        return result;
+    }
+
+    @Override
+
+    public String toString() {
+        System.out.println(this.getName());
+        System.out.println("Area: " + width*height);
+        return null;
+    }
 }
 
 class Square extends Rectangle{
