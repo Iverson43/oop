aaed2c236fbfac3ccaab085e83f33cca8d4a109a
borisPMC <borisleung30@gmail.com>
2021-11-08 15:08:33 +0800

    change back JDK 11

--- SampleProject/SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/model/Shape.java
+++ SampleProject/SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/model/Shape.java
@@ -1,6 +1,6 @@
 package hk.edu.polyu.comp.comp2021.clevis.model;
 
-public class Shape {
+class Shape {
 
     // The name and anchor of a Shape
     private String name;
@@ -19,3 +19,57 @@ public class Shape {
     double getStartY() {return y;}
 }
 
+class Line extends Shape{
+
+    private double endX;
+    private double endY;
+
+    Line(String n, double x1, double y1, double x2, double y2) {
+        super(n, x1, y1);
+        this.endX = x2;
+        this.endY = y2;
+    }
+
+    // Returns width and height of a Rectangle
+    double getEndX() {return endX;}
+    double getEndY() {return endY;}
+}
+
+class Circle extends Shape{
+
+    //The radian of a circle
+    private double radian;
+
+    Circle(String n, double x, double y, double r) {
+        super(n, x, y);
+        this.radian = r;
+    }
+
+    //Return the radian of a circle
+    double getRadian() {return radian;}
+}
+
+class Rectangle extends Shape {
+
+    // The height and width for a rectangle.
+    private double width;
+    private double height;
+
+    Rectangle(String n, double x, double y, double w, double h) {
+        super(n, x, y);
+        this.width = w;
+        this.height = h;
+    };
+
+    // Returns width and height of a Rectangle
+    double getWidth() {return width;}
+    double getHeight() {return height;}
+}
+
+class Square extends Rectangle{
+
+    Square(String n, double x, double y, double l) {
+        super(n, x, y, l, l);
+    }
+
+}
--- SampleProject/SampleProject/src/hk/edu/polyu/comp/comp2021/clevis/model/Square.java
+++
@@ -1,9 +0,0 @@
-package hk.edu.polyu.comp.comp2021.clevis.model;
-
-public class Square extends Rectangle{
-
-    Square(String n, double x, double y, double l) {
-        super(n, x, y, l, l);
-    }
-    
-}
